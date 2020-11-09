0b47e4912ccae0eb18261fbd20bfe2185533db14
Bailey <baileylritchie@gmail.com>
2020-11-08 21:13:28 -0600

    connecting book objects in script to frontend

--- script.js
+++ script.js
@@ -21,6 +21,31 @@ Book.prototype.toggleRead = function () {
     this.read = false;
   } else this.read = true;
 }
-let book1 = new Book('hi there','me',123,true);
 
-console.log(Object.getPrototypeOf(book1));
+function createBookHTML(bookObj) {
+  const library = document.querySelector('.library');
+  library.innerHTML += 
+    `<div class="book-card">
+      <div class="book-prop">${bookObj.title} </div>
+      <div class="book-prop">${bookObj.author}</div>
+      <div class="book-prop">${bookObj.pages}</div>
+      <div class="book-prop">${bookObj.read}</div>
+      <button>Mark as Read</button>
+    </div>
+    `;
+}
+function addBookToLibrary(){
+  // create the function when form submission is available
+}
+
+function initializeLibrary(){
+  // this is where we will map the books in myLibrary to the books in HTML
+  myLibrary.forEach((book) => {
+    createBookHTML(book);
+  });
+}
+let book1 = new Book('test','bailey',123,true);
+myLibrary.push(book1);
+initializeLibrary();
+
+console.log(myLibrary);
